e1f2ad01c78076b02a7a89f6d672f99d785cf8c7
Danang Sukma <danangsukma@gmail.com>
2019-02-06 10:33:19 +0700

    Create azure-powershell-tab-completer.sh

---
+++ azure-powershell-tab-completer.sh
@@ -0,0 +1,4 @@
+#Since version 3.0, PowerShell has supported applying argument completers to cmdlet parameters. 
+#To best capture the functionality of the completers, Modify the key binding for “Tab” in the examples below to display all the possible values at once.
+#Simply run:
+Set-PSReadLineKeyHandler -Key Tab -Function Complete
