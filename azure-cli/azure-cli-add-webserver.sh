fb334bb8ac7ace78aa159347f6b067d7430682f9
Danang Sukma <danangsukma@gmail.com>
2019-04-22 14:45:04 +0700

    Update azure-cli-add-webserver.sh

--- azure-cli/azure-cli-add-webserver.sh
+++ azure-cli/azure-cli-add-webserver.sh
@@ -1,5 +1,15 @@
 #Configure IIS, download and execute a PowerShell script that installs IIS and configures a basic home page.
+#Windows
 az vm extension set --resource-group [Your Resource Group Name] --vm-name [Your VM Name] --name CustomScriptExtension --publisher Microsoft.Compute --settings "{'fileUris':['https://raw.githubusercontent.com/MicrosoftDocs/mslearn-welcome-to-azure/master/configure-iis.ps1']}" --protected-settings "{'commandToExecute': 'powershell -ExecutionPolicy Unrestricted -File configure-iis.ps1'}"
+
+#Linux
+az vm extension set \
+  --resource-group 00807a57-e905-46cc-a1c2-ee3eb5e52038 \
+  --vm-name myVM \
+  --name customScript \
+  --publisher Microsoft.Azure.Extensions \
+  --settings "{'fileUris':['https://raw.githubusercontent.com/MicrosoftDocs/mslearn-welcome-to-azure/master/configure-nginx.sh']}" \
+  --protected-settings "{'commandToExecute': './configure-nginx.sh'}"
 
 #Open port 80 (HTTP) through the firewall.
 az vm open-port --name [Your VM Name] --resource-group [Your Resource Group Name] --port 80
