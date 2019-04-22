790822176fee8c252d773fc408fbc5bedcb22a61
Danang Sukma <danangsukma@gmail.com>
2019-04-22 12:31:03 +0700

    Create allocate -az-firewall.ps1

---
+++ azure-powershell/allocate -az-firewall.ps1
@@ -0,0 +1,7 @@
+# Start a firewall
+
+$azfw = Get-AzFirewall -Name "FW Name" -ResourceGroupName "RG Name"
+$vnet = Get-AzVirtualNetwork -ResourceGroupName "RG Name" -Name "VNet Name"
+$publicip = Get-AzPublicIpAddress -Name "Public IP Name" -ResourceGroupName " RG Name"
+$azfw.Allocate($vnet,$publicip)
+Set-AzFirewall -AzureFirewall $azfw
